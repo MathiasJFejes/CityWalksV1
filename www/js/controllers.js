068acfcf65816e80faf19cd3e848a196b30b7ac1
MathiasJFejes <mathias.johannes.fejes@gmail.com>
2017-04-05 09:41:23 +0200

    Update www files
    new templates and directories

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
   
-.controller('startTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
@@ -8,22 +8,6 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('routsTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-   
-.controller('profileTabDefaultPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
-}])
-      
 .controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,4 +1,4 @@
-angular.module('app.routes', ['ionicUIRouter'])
+angular.module('app.routes', [])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -10,80 +10,25 @@ angular.module('app.routes', ['ionicUIRouter'])
     
   
 
-      /* 
-    The IonicUIRouter.js UI-Router Modification is being used for this route.
-    To navigate to this route, do NOT use a URL. Instead use one of the following:
-      1) Using the ui-sref HTML attribute:
-        ui-sref='tabsController.startTabDefaultPage'
-      2) Using $state.go programatically:
-        $state.go('tabsController.startTabDefaultPage');
-    This allows your app to figure out which Tab to open this page in on the fly.
-    If you're setting a Tabs default page or modifying the .otherwise for your app and
-    must use a URL, use one of the following:
-      /page1/tab1/page2
-      /page1/tab3/page2
-  */
-  .state('tabsController.startTabDefaultPage', {
-    url: '/page2',
-    views: {
-      'tab1': {
-        templateUrl: 'templates/startTabDefaultPage.html',
-        controller: 'startTabDefaultPageCtrl'
-      },
-      'tab3': {
-        templateUrl: 'templates/startTabDefaultPage.html',
-        controller: 'startTabDefaultPageCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.routsTabDefaultPage', {
-    url: '/page3',
-    views: {
-      'tab2': {
-        templateUrl: 'templates/routsTabDefaultPage.html',
-        controller: 'routsTabDefaultPageCtrl'
-      }
-    }
-  })
-
-  .state('tabsController.profileTabDefaultPage', {
-    url: '/page4',
-    views: {
-      'tab3': {
-        templateUrl: 'templates/profileTabDefaultPage.html',
-        controller: 'profileTabDefaultPageCtrl'
-      }
-    }
-  })
-
-  .state('tabsController', {
+      .state('homePage', {
     url: '/page1',
-    templateUrl: 'templates/tabsController.html',
-    abstract:true
+    templateUrl: 'templates/homePage.html',
+    controller: 'homePageCtrl'
   })
 
-  .state('tabsController.login', {
-    url: '/page5',
-    views: {
-      'tab3': {
-        templateUrl: 'templates/login.html',
-        controller: 'loginCtrl'
-      }
-    }
+  .state('login', {
+    url: '/page2',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
   })
 
-  .state('tabsController.signup', {
-    url: '/page6',
-    views: {
-      'tab3': {
-        templateUrl: 'templates/signup.html',
-        controller: 'signupCtrl'
-      }
-    }
+  .state('signup', {
+    url: '/page3',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
   })
 
-$urlRouterProvider.otherwise('/page1/page5')
+$urlRouterProvider.otherwise('/page2')
 
   
 
